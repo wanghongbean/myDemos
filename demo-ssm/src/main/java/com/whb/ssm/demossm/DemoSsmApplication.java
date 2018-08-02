ec2f19fdcf7dd0a9304eb919d9054a44c4511336
Whongb <625688265@qq.com>
2018-08-02 23:40:11 +0800

    demo-ssm

---
+++ demo-ssm/src/main/java/com/whb/ssm/demossm/DemoSsmApplication.java
@@ -0,0 +1,12 @@
+package com.whb.ssm.demossm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSsmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSsmApplication.class, args);
+	}
+}
